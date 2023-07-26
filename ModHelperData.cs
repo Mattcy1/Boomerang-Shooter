efee62a07d9a98ed80a73d7be583a1b54e5ed02b
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2023-07-26 12:51:09 +0200

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,15 @@
+﻿namespace BoomerangShooter
+{
+    public static class ModHelperData
+    {
+        public const string WorksOnVersion = "37.0";
+        public const string Version = "1.2";
+        public const string Name = "Boomerang Shooter";
+        public const string Description =
+            "Tack Shooter are boring so i made a Boomerang Shooter Also Check Out My Youtube Channel Its MattcyYT with the litte orange and blue icon thanks!";
+
+        public const string RepoOwner = "Mattcy1";
+        public const string RepoName = "Boomerang-shooter";
+        public const string Icon = "BoomerangShooter-Icon.png";
+    }
+}
